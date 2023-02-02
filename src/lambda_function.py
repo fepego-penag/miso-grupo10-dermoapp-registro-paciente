226df8d67fb0c35b84aa2e827934ad73d4546a9d
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-01 22:45:36 -0500

    adding duplication

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -13,3 +13,14 @@ def lambda_handler(event, context):
             }
         ),
     }
+
+def lambda_handler_duplication(event, context):
+    x = requests.get('https://w3schools.com/python/demopage.htm')
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello from CI GIT pipline dev!" + x.text,
+            }
+        ),
+    }
