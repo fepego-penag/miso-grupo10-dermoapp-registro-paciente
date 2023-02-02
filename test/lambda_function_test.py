12d99c6a751b6c62434bde50d7393522cc275223
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-01 22:56:55 -0500

    testing code smell failure

--- test/lambda_function_test.py
+++ test/lambda_function_test.py
@@ -1,8 +1,16 @@
-from src.lambda_function import lambda_handler
+from src.lambda_function import *
 
 
 class TestPacientDetailService:
 
     def test_givenLambdaFunctionCallThenReturnJsonResponse(self):
         serviceResponse = lambda_handler(any, any)
+        assert serviceResponse['statusCode'] == 200
+
+    def test_givenLambdaFunctionCallThenReturnJsonResponse2(self):
+        serviceResponse = lambda_handler_duplication(any, any)
+        assert serviceResponse['statusCode'] == 200
+
+    def test_givenLambdaFunctionCallThenReturnJsonResponse2(self):
+        serviceResponse = lambda_handler_duplication2(any, any)
         assert serviceResponse['statusCode'] == 200
