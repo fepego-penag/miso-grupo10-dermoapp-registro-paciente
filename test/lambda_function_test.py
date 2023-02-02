61fa47ea8e53e4b327330ff841ed8d6ae21bf871
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-01 22:27:24 -0500

    testing failure coverture

--- test/lambda_function_test.py
+++ test/lambda_function_test.py
@@ -3,6 +3,5 @@ from src.lambda_function import lambda_handler
 
 class TestPacientDetailService:
 
-    def test_givenLambdaFunctionCallThenReturnJsonResponse(self):
-        serviceResponse = lambda_handler(any, any)
-        assert serviceResponse['statusCode'] == 200
+    def test_nothing(self):
+        assert 1==1
